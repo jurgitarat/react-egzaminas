637497be42b0603fa9573a030c44ae137cfa5f0a
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-11-16 20:47:21 +0200

    Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import React from 'react';
 import { useState } from "react";
-//import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { RegisterPage } from './views/RegisterPage/RegisterPage';
-import { HomePage } from './views/HomePage/HomePage';
-import { LoginPage } from './views/LoginPage/LoginPage';
-import { AddPage } from './views/AddPage/AddPage';
+import { RouteSuspense } from './components/RouteSuspense/RouteSuspense';
 import { Navigation } from './components/navi/navi';
-// const HomePage = React.lazy(() => import('./views/HomePage/HomePage'));
-//const RegisterPage = React.lazy(() => import('./views/RegisterPage/RegisterPage'));
-// const LoginPage = React.lazy(() => import('./views/LoginPage/LoginPage'));
-// const AddPage = React.lazy(() => import('./views/AddPage/AddPage'));
+
+const HomePage = React.lazy(() => import('./views/HomePage/HomePage'));
+const LoginPage = React.lazy(() => import('./views/LoginPage/LoginPage'));
+const AddPage = React.lazy(() => import('./views/AddPage/AddPage'));
+const RegisterPage = React.lazy(() => import('./views/RegisterPage/RegisterPage'));
 
 function App() {
   const [userToken, setUserToken] = useState(null);
   const handleLogin = (token) => {
     setUserToken(token)
   };
-  
   return (
     <div className="App">
       
       <Navigation  token={userToken}  />
     
       <Routes>
-        <Route path="/" element={<HomePage token={userToken} />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage  onLogin={handleLogin}  />} />
-        <Route path="/add" element={<AddPage token={userToken} />} />
+        <Route path="/" element={
+        
+        <RouteSuspense>
+        <HomePage token={userToken} />
+        
+        </RouteSuspense>
+        } />
+        <Route path="/register" element={
+        
+        <RouteSuspense>
+        <RegisterPage />
+        
+        </RouteSuspense>
+        } />
+        <Route path="/login" element={
+        
+        <RouteSuspense>
+        <LoginPage  onLogin={handleLogin}  />
+        
+        </RouteSuspense>
+        
+        } />
+        <Route path="/add" element={
+        
+        <RouteSuspense>
+        <AddPage token={userToken} />
+        
+        </RouteSuspense>
+        
+        } />
       </Routes>
 
 
